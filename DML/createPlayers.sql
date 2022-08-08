19dc98a72dd6e9f89cc4610963d6e9ed8407d8ad
Gabriela <gabi16.hernandez@gmail.com>
2022-08-08 16:09:44 -0600

    Registros para la base

---
+++ DML/createPlayers.sql
@@ -0,0 +1,59 @@
+--CREATE TYPES OF GAMES
+INSERT INTO typeGame (id_typeGame, typeGame) VALUES (001,'HangMan');
+INSERT INTO typeGame (id_typeGame, typeGame) VALUES (002,'TicTacToe');
+
+
+--CREATE TYPES OF PLAYERS
+INSERT INTO typeofplayer (id_typeofplayers, typeofplayer) VALUES (001,'HangManPlayer');
+INSERT INTO typeofplayer (id_typeofplayers, typeofplayer) VALUES (002,'TicTacToePlayer');
+
+--CREATE PLAYERS
+INSERT INTO player (id_player,player_name, win_points,loose_points, id_typegame,  id_typeofplayers)
+VALUES (001,'Carlos',100,0,001,001);
+INSERT INTO player (id_player,player_name, win_points,loose_points, id_typegame,  id_typeofplayers)
+VALUES (002,'Roberto',120,20,001,001);
+
+INSERT INTO player (id_player,player_name, win_points,loose_points, id_typegame,  id_typeofplayers)
+VALUES (003,'Maria',100,25,002,002);
+INSERT INTO player (id_player,player_name, win_points,loose_points, id_typegame,  id_typeofplayers)
+VALUES (004,'Alejandra',0,125,002,002);
+
+
+--INSERT VALUES ON TABLE WORD
+INSERT INTO word (id_word,secretword,guessedword,player_id_player,id_typeofplayers) 
+VALUES (001,'Friday','Fri',001,001) ;
+INSERT INTO word (id_word,secretword,guessedword,player_id_player,id_typeofplayers) 
+VALUES (002,'Hello','Hel',002,001) ;
+
+INSERT INTO word (id_word,secretword,guessedword,player_id_player,id_typeofplayers) 
+VALUES (003,'Sun','Sun',002,001) ;
+INSERT INTO word (id_word,secretword,guessedword,player_id_player,id_typeofplayers) 
+VALUES (004,'Letter','Let',001,001) ;
+
+
+--INSERT VALUES ON TABLE BOARD
+INSERT INTO BOARD (id_board,freePossitions) VALUES (001,9);
+INSERT INTO BOARD (id_board,freePossitions) VALUES (002,6);
+INSERT INTO BOARD (id_board,freePossitions) VALUES (003,2);
+
+UPDATE board  SET board.freepossitions =3 WHERE board.id_board = 002;
+
+--TABLE PIECE
+INSERT INTO piece (id_piece, piece,player_id_player,id_typeofplayers) VALUES (001,'O',003,002);
+INSERT INTO piece (id_piece, piece,player_id_player,id_typeofplayers) VALUES (002,'X',004,002);
+
+--TABLE ROUND
+INSERT INTO ROUND (id_round,player_id_player,player_id_typeofplayers) VALUES (001,001,001);
+INSERT INTO ROUND (id_round,player_id_player,player_id_typeofplayers) VALUES (002,002,001);
+INSERT INTO ROUND (id_round,player_id_player,player_id_typeofplayers) VALUES (003,003,002);
+INSERT INTO ROUND (id_round,player_id_player,player_id_typeofplayers) VALUES (004,004,002);
+
+
+--TABLE TURN
+INSERT INTO TURN (id_turn,round_id_round) VALUES (001,001);
+INSERT INTO TURN (id_turn,round_id_round) VALUES (002,001);
+INSERT INTO TURN (id_turn,round_id_round) VALUES (003,001);
+
+INSERT INTO TURN (id_turn,round_id_round) VALUES (004,002);
+INSERT INTO TURN (id_turn,round_id_round) VALUES (005,002);
+INSERT INTO TURN (id_turn,round_id_round) VALUES (006,002);
